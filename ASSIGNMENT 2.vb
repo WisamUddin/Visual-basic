cb5282cf713b603ef8cc8dab039fad8f2988fbde
Muhammad Wisam Uddin <70229220+WisamUddin@users.noreply.github.com>
2021-11-14 07:43:16 -0800

    Add files via upload

---
+++ ASSIGNMENT 2.vb
@@ -0,0 +1,42 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim A, B, C, D, E As String
+        Dim count As Integer
+
+        A = ""
+        B = ""
+
+        C = ""
+        D = ""
+        E = ""
+        count = 0
+
+        Console.Write("Enter Your Sentence: ")
+        A = Console.ReadLine
+        Console.Write("Enter Char you want to replace")
+        C = Console.ReadLine
+        Console.Write("Enter Char you want to replace with: ")
+        D = Console.ReadLine
+
+        For count = 1 To Len(A)
+            E = Mid(A, count, 1)
+            Dim message As String = ""
+            message = IIf(B = "B", "E")
+
+            Console.WriteLine("we are unable to complete your task" & message)
+            Console.ReadLine()
+            Console.ReadKey()
+
+
+
+        Next
+
+
+    End Sub
+
+    Private Function IIf(ByVal p1 As Boolean, ByVal p2 As String) As String
+
+    End Function
+
+End Module
