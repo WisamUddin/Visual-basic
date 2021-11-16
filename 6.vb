0deebb249b24fcd2c9250da3bf7bc9eeb72d8941
Muhammad Wisam Uddin <70229220+WisamUddin@users.noreply.github.com>
2021-11-16 06:56:29 -0800

    Add files via upload

---
+++ 6.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim i As String
+        i = ""
+
+        Console.Write("Enter the number of Donuts you want: ")
+        i = Console.ReadLine()
+
+
+        If i < 10 Then
+
+            Console.WriteLine("Numer of Donuts are  " & i)
+        Else
+            Console.WriteLine("Number of donuts are Many")
+        End If
+
+
+    End Sub
+
+End Module
