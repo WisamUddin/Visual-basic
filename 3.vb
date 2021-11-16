8dd496aadd157d4ec844525adf4659555ab199bd
Muhammad Wisam Uddin <70229220+WisamUddin@users.noreply.github.com>
2021-11-16 06:28:50 -0800

    Update 3.vb

--- 3.vb
+++ 3.vb
@@ -1,15 +1,16 @@
 ﻿Module Module1
 
     Sub Main()
+    Dim A, Char1, B As String
+        Dim i, cCount As Integer
 
-        Dim A, Char1, B As String
-        Dim Counter, cCount As Integer
+        i = 0
+        cCount = 0
 
         A = ""
+
         Char1 = ""
         B = ""
-        Counter = 0
-        cCount = 0
 
         Console.Write("Enter a String ")
         A = Console.ReadLine
@@ -17,8 +18,8 @@
         Console.Write("Enter character to search for: ")
         B = Console.ReadLine
 
-        For Counter = 1 To Len(A)
-            Char1 = Mid(A, Counter, 1)
+        For i = 1 To Len(A)
+            Char1 = Mid(A, i, 1)
             If UCase(Char1) = UCase(B) Then
                 cCount = cCount + 1
             End If
